6ff3ff598e832d266661ab472d397da03f6feddb
christmas-cookie <smellinggrassdaily@gmail.com>
2026-06-21 16:55:18 +0300

    update 👀 english module

--- src/translations/English.lua
+++ src/translations/English.lua
@@ -1,3 +1,15 @@
 return {
-    Example = "Hello"
+    -- tabs --
+    MainTabTitle = "Main",
+    MainTabDescription = "General info and community links.",
+    PlayerTabTitle = "Player",
+    PlayerTabDescription = "Features that are dependent on you.",
+    VisualsTabTitle = "Visuals",
+    VisualsTabDescription = "Things that only you can see on your screen.",
+    AutomationTabTitle = "Automation",
+    AutomationTabDescription = "Features that run automatically for you.",
+    FunTabTitle = "Fun",
+    FunTabDescription = "Stuff for messing around and having fun.",
+    SettingsTabTitle = "Settings",
+    SettingsTabDescription = "Options to change how the UI works."
 }
