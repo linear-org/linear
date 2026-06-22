5f88d79b9518a151f969128052ccaab4cc9d02ad
olivia1246 <135425765+olivia1246@users.noreply.github.com>
2026-06-22 13:13:57 +0300

    russian translation (my russian skills are TERRIBLE most of this is with translator checked over by me)

---
+++ src/translations/Russian.lua
@@ -0,0 +1,15 @@
+return {
+    -- tabs --
+    MainTabTitle = "Главная",
+    MainTabDescription = "Общая информация и ссылки на сообщества.",
+    PlayerTabTitle = "Игрок",
+    PlayerTabDescription = "Функции, которые зависят от вас.",
+    VisualsTabTitle = "Визуальные",
+    VisualsTabDescription = "То, что видно только вам на экране.",
+    AutomationTabTitle = "Автоматизация",
+    AutomationTabDescription = "Функции, которые запускаются автоматически.",
+    FunTabTitle = "Веселье",
+    FunTabDescription = "Вещи, с которыми можно подурачиться и повеселиться.",
+    SettingsTabTitle = "Настройки",
+    SettingsTabDescription = "Настройки, позволяющие настроить пользовательский интерфейс."
+}
