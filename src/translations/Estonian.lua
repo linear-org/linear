623594087319352fa99ab623e073288a692a0b48
olivia1246 <135425765+olivia1246@users.noreply.github.com>
2026-06-22 13:07:59 +0300

    estonian translation

---
+++ src/translations/Estonian.lua
@@ -0,0 +1,15 @@
+return {
+    -- tabs --
+    MainTabTitle = "Peamine",
+    MainTabDescription = "Üldine teave ja kogukonna lingid.",
+    PlayerTabTitle = "Mängija",
+    PlayerTabDescription = "Funktsioonid, mis sõltuvad sinust.",
+    VisualsTabTitle = "Visuaalid",
+    VisualsTabDescription = "Asjad, mida ainult sina oma ekraanil näed.",
+    AutomationTabTitle = "Automatiseerimine",
+    AutomationTabDescription = "Funktsioonid, mis töötavad teie eest automaatselt.",
+    FunTabTitle = "Lõbus",
+    FunTabDescription = "Asjad, millega mängida ja lõbutseda.",
+    SettingsTabTitle = "Seaded",
+    SettingsTabDescription = "Valikud kasutajaliidese toimimise muutmiseks."
+}
