2ca9bcd8ddcf5e4502e3f745ea8d151c43ef03fe
christmas-cookie <smellinggrassdaily@gmail.com>
2026-06-26 23:00:24 +0300

    Fix English.lua

--- src/translations/English.lua
+++ src/translations/English.lua
@@ -59,10 +59,23 @@ return {
     TakeOneItemDesc = "Takes one singular item if found.",
     TakeAllItemsTitle = "Take All Items",
     TakeAllItemsDesc = "Takes every item if found.",
+    
+    TakeOneStuffingPileTitle = "Take One Stuffing Pile",
+    TakeOneStuffingPileDesc = "Takes one singular stuffing pile if found.",
+    TakeAllStuffingPilesTitle = "Take All Stuffing Piles",
+    TakeAllStuffingPilesDesc = "Takes every stuffing pile if found.",
+   
     GriefOneItemTitle = "Grief One Item",
     GriefOneItemDesc = "Takes one of the items on the map and teleports it to somewhere inaccessible.",
     GriefAllItemsTitle = "Grief All Items",
     GriefAllItemsDesc = "Takes all of the items on the map and teleports them to somewhere inaccessible.",
+    
+    ProtectionGroupboxTitle = "Protection",
+    ProtectionGroupboxDesc = "Utilities about protection.",
+    ImmunityRejectsTitle = "Rejects God Mode",
+    ImmunityRejectsDesc = "Makes you immune to the rejects, making them not be able to see you neither attack you if they see you so.",
+    ImmunityMeistroTitle = "Meistro God Mode",
+    ImmunityMeistroDesc = "Makes you immune to Meistro's attacks.",
     
     -- visuals --
     PerformanceGroupboxTitle = "Performance",
@@ -84,6 +97,12 @@ return {
     AutofarmGroupboxDesc = "Plays the game for you.",
     ToggleAutofarmTitle = "Toggle Autofarm",
     ToggleAutofarmDesc = "Toggles the auto-farm, if you want to save peformance to prevent disconnecting and overheating, you can turn on the ‘Disable Rendering’ feature located in Visuals > Performance.",
+    Automation_UtilitiesGroupboxTitle = "Utilities",
+    Automation_UtilitiesGroupboxDesc = "Overall utilities automated.",
+    AutoTakeStuffingPileTitle = "Auto Take Stuffing Piles",
+    AutoTakeStuffingPileDesc = "Automatically takes the stuffing piles on panic.",
+    AutoTeleportTrainTitle = "Auto Teleport To Train",
+    AutoTeleportTrainDesc = "Automatically teleports to the train on panic.",
 --[[
     -- visuals --
     PerformanceGroupboxTitle = "Performance",
