af5f663a022fc8f9d1cd7a3d13be43074bf79f98
christmas-cookie <smellinggrassdaily@gmail.com>
2026-06-26 22:26:42 +0300

    top secret

---
+++ loader.lua
@@ -0,0 +1,14 @@
+local textchatservice = game:GetService("TextChatService")
+local rbxsystem = textchatservice:WaitForChild("TextChannels"):WaitForChild("RBXSystem")
+
+local function chat(from, what, fromcolor, whatcolor)
+    local msg = string.format("<font color='rgb(%.0f,%.0f,%.0f)'>[%s]:</font> <font color='rgb(%.0f,%.0f,%.0f)'>%s</font>", fromcolor.R * 255, fromcolor.G * 255, fromcolor.B * 255, from, whatcolor.R * 255, whatcolor.G * 255, whatcolor.B * 255, what)
+    rbxsystem:DisplaySystemMessage(msg)
+end
+
+if game.PlaceId == 72515936648142 then
+    chat("linear", "This game is supported! ‘Dolly's Factory: In-Game’, loading the script..", Color3.fromRGB(20, 20, 20), Color3.fromRGB(255, 255, 255))
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/linear-org/linear/refs/heads/main/src/dollysfactory/main.lua"))()
+else
+    chat("linear", "This game isn't supported..", Color3.fromRGB(20, 20, 20), Color3.fromRGB(255, 255, 255))
+end
