4991ddc3a051376ecef2f2aa5147c03a02be52a6
christmas-cookie <smellinggrassdaily@gmail.com>
2026-07-01 02:34:28 +0300

    Add files via upload

---
+++ src/translations/English.lua
@@ -0,0 +1,79 @@
+return { 
+	-- Tabs
+	MainTabTitle = "Main",
+	PlayerTabTitle = "Player",
+	AutomationTabTitle = "Automation",
+	
+	-- Main
+	WelcomeTitle = "Great to see you" ,
+	WelcomeDesc = "Hope your day’s going good!",
+	
+	AnonymousModeTitle = "Remain Anonymous",
+	AnonymousModeDesc = "Hides your username throughout the script.",
+	
+	CreditsTitle = "Credits",
+	CreditsDesc = "Awesome people list!",
+	Credits_GrassyDesc = "The founder and the owner of linear.",
+	Credits_OliviaDesc = "The owner of Riddance, who also helps <b>a lot</b> with linear's development, thanks to them we got most of the features to work!",
+	
+	-- Player
+	MovementGroupboxTitle = "Movement",
+	MovementGroupboxDesc = "Features about your movement.",
+	
+	InfiniteStaminaTitle = "Infinite Stamina",
+	InfiniteStaminaDesc = "Prevents stamina usage, making you have endless stamina.",
+    NoclipTitle = "Noclip",
+    NoclipDesc = "Lets you go through objects.",
+    WalkingSpeedSliderTitle = "Walking Speed",
+    WalkingSpeedSliderDesc = "What your new walking speed should be.",
+    RunningSpeedSliderTitle = "Running Speed",
+    RunningSpeedSliderDesc = "What your new running speed should be.",
+    OverrideSpeedTitle = "Overwrite Speed",
+    OverrideSpeedDesc = "Changes your walking & running speed to the values you've chosen.",
+    
+    ItemsGroupboxTitle = "Item",
+    ItemsGroupboxDesc = "Features about in-game items.",
+    
+    ItemRarityDropdownTitle = "Item Rarity",
+    ItemRarityDropdownDesc = "Choose an item rarity to use these features on.",
+    TakeOneItemTitle = "Take One Item",
+    TakeOneItemDesc = "Takes one of the items in the map with the chosen rarity.",
+    TakeAllItemsTitle = "Take All Items",
+    TakeAllItemsDesc = "Takes all of the items in the map with the chosen rarity.",
+    GriefOneItemTitle = "Grief One Item",
+    GriefOneItemDesc "Takes one of the items in the map with the chosen rarity and then teleports it to somewhere inaccessible.",
+    GriefAllItemsTitle = "Grief All Items",
+    GriefAllItemsDesc = "Takes all of the items in the map with the chosen rarity and then teleports it to somewhere inaccessible.",
+    
+    SafetyGroupboxTitle = "Protection",
+    SafetyGroupboxDesc = "Features about your safety.",
+    
+    OffsetThresholdTitle = "Offset Threshold",
+    OffsetThresholdDesc = "The offset value for the rejects god mode, it is preferred to turn on Smart Offset under this slider instead of playing with this value.",
+    ImmunityRejectsTitle = "Rejects God Mode",
+    ImmunityRejectsDesc = "Makes you immune to every reject and their attacks.",
+    ImmunityMeistroTitle = "Meistro God Mode",
+    ImmunityMeistroDesc = "Makes you immune to Meistro's attacks.",
+    AutoHiFlyTitle = "HiFly God Mode",
+    AutoHiFlyDesc = "Makes so you automatically like HiFly's peak music.",
+    
+    UtilityGroupboxTitle = "Utility",
+    UtilityGroupboxDesc = "Features about game utilities.",
+    DesyncTitle = "Desync",
+    DesyncDesc = "Makes so people still see you .",
+    
+    -- Automation
+    CalibrationGroupboxTitle = "Calibration",
+    CalibrationGroupboxDesc = "Features about extracting machines.",
+    AutoDefaultMachTitle = "Auto Calibrate Default Machines",
+    AutoDefaultMachDesc = "Automatically does the skillcheck for default machines.",
+    AutoDroneMachTitle = "Auto Calibrate Drone Machines",
+    AutoDroneMachDesc = "Automatically does the skillcheck for drone machines.",
+    
+    ToggleAutofarmTitle = "Toggle Autofarm",
+    ToggleAutofarmDesc = "Automatically plays the game for you, expect this feature to have minor bugs.",
+    
+    -- TEMPORARY
+    WhereAreStuffTitle = "WHERE'S MOST OF THE SCRIPT!?",
+    WhereAreStuffDesc = "This version only supports Main, Player & Automation tabs, the others are still in progress, blame the owner for being a lazy fuck. This version is highly unstable, report the bugs you find in the tester bug reports, where else you'd get this script anyways.\n\n I may just quit."
+}
