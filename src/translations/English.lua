64d348b331e9e3e665ea9600ce083c50647d4d6e
christmas-cookie <smellinggrassdaily@gmail.com>
2026-07-01 02:30:33 +0300

    Update English.lua

--- src/translations/English.lua
+++ src/translations/English.lua
@@ -1,126 +1,79 @@
---[[
-   HIGHLY UNSTABLE VERSION
-!!!      DO NOT TRANSLATE       !!!
-]]
-
-return {
-    -- tabs --
-    MainTabTitle = "Main",
-    PlayerTabTitle = "Player",
-    VisualsTabTitle = "Visuals",
-    AutomationTabTitle = "Automation",
-    FunTabTitle = "Fun",
-    DonorTabTitle = "Donor",
-    SettingsTabTitle = "Settings",
-    
-    -- player --
-    MovementGroupboxTitle = "Movement",
-    MovementGroupboxDesc = "Character based utilities.",
-    InfiniteStaminaTitle = "Infinite Stamina",
-    InfiniteStaminaDesc = "Makes so you never run out of stamina.",
-    WalkingSpeedSliderTitle = "Walking Speed",
-    WalkingSpeedSliderDesc = "What shall your walking speed be.",
-    RunningSpeedSliderTitle = "Running Speed",
-    RunningSpeedSliderDesc = "What shall your running speed be.",
-    OverrideSpeedTitle = "Overwrite Speed",
-    OverrideSpeedDesc = "Applies the new walking & speeds you've chosen.",
+return { 
+	-- Tabs
+	MainTabTitle = "Main",
+	PlayerTabTitle = "Player",
+	AutomationTabTitle = "Automation",
+	
+	-- Main
+	WelcomeTitle = "Great to see you" ,
+	WelcomeDesc = "Hope your day’s going good!",
+	
+	AnonymousModeTitle = "Remain Anonymous",
+	AnonymousModeDesc = "Hides your username throughout the script.",
+	
+	CreditsTitle = "Credits",
+	CreditsDesc = "Awesome people list!",
+	Credits_GrassyDesc = "The founder and the owner of linear.",
+	Credits_OliviaDesc = "The owner of Riddance, who also helps <b>a lot</b> with linear's development, thanks to them we got most of the features to work!",
+	
+	-- Player
+	MovementGroupboxTitle = "Movement",
+	MovementGroupboxDesc = "Features about your movement.",
+	
+	InfiniteStaminaTitle = "Infinite Stamina",
+	InfiniteStaminaDesc = "Prevents stamina usage, making you have endless stamina.",
     NoclipTitle = "Noclip",
-    NoclipDesc = "Let's you go through walls.",
+    NoclipDesc = "Lets you go through objects.",
+    WalkingSpeedSliderTitle = "Walking Speed",
+    WalkingSpeedSliderDesc = "What your new walking speed should be.",
+    RunningSpeedSliderTitle = "Running Speed",
+    RunningSpeedSliderDesc = "What your new running speed should be.",
+    OverrideSpeedTitle = "Overwrite Speed",
+    OverrideSpeedDesc = "Changes your walking & running speed to the values you've chosen.",
     
+    ItemsGroupboxTitle = "Item",
+    ItemsGroupboxDesc = "Features about in-game items.",
     
-    ItemsGroupboxTitle = "Items",
-    ItemsGroupboxDesc = "Items based utilities.",
-    
-    TakeOneJunkItemTitle = "Take One Junk Item",
-    TakeOneJunkItemDesc = "Takes one singular item with the ‘Junk’ rarity if found.",
-    TakeAllJunkItemsTitle = "Take All Junk Items",
-    TakeAllJunkItemsDesc = "Takes every item with the ‘Junk’ rarity if found.",
-    TakeOneCommonItemTitle = "Take One Common Item",
-    TakeOneCommonItemDesc = "Takes one singular item with the ‘Common’ rarity if found.",
-    TakeAllCommonItemsTitle = "Take All Common Items",
-    TakeAllCommonItemsDesc = "Takes every item with the ‘Common’ rarity if found.",
-    TakeOneUncommonItemTitle = "Take One Uncommon Item",
-    TakeOneUncommonItemDesc = "Takes one singular item with the ‘Uncommon’ rarity if found.",
-    TakeAllUncommonItemsTitle = "Take All Uncommon Items",
-    TakeAllUncommonItemsDesc = "Takes every item with the ‘Uncommon’ rarity if found.",
-    TakeOneRareItemTitle = "Take One Rare Item",
-    TakeOneRareItemDesc = "Takes one singular item with the ‘Rare’ rarity if found.",
-    TakeAllRareItemsTitle = "Take All Rare Items",
-    TakeAllRareItemsDesc = "Takes every item with the ‘Rare’ rarity if found.",
-    TakeOneEpicItemTitle = "Take One Epic Item",
-    TakeOneEpicItemDesc = "Takes one singular item with the ‘Epic’ rarity if found.",
-    TakeAllEpicItemsTitle = "Take All Epic Items",
-    TakeAllEpicItemsDesc = "Takes every item with the ‘Epic’ rarity if found.",
-    TakeOneLegendaryItemTitle = "Take One Legendary Item",
-    TakeOneLegendaryItemDesc = "Takes one singular item with the ‘Legendary’ rarity if found.",
-    TakeAllLegendaryItemsTitle = "Take All Legendary Items",
-    TakeAllLegendaryItemsDesc = "Takes every item with the ‘Legendary’ rarity if found.",
+    ItemRarityDropdownTitle = "Item Rarity",
+    ItemRarityDropdownDesc = "Choose an item rarity to use these features on.",
     TakeOneItemTitle = "Take One Item",
-    TakeOneItemDesc = "Takes one singular item if found.",
+    TakeOneItemDesc = "Takes one of the items in the map with the chosen rarity.",
     TakeAllItemsTitle = "Take All Items",
-    TakeAllItemsDesc = "Takes every item if found.",
-    
-    TakeOneStuffingPileTitle = "Take One Stuffing Pile",
-    TakeOneStuffingPileDesc = "Takes one singular stuffing pile if found.",
-    TakeAllStuffingPilesTitle = "Take All Stuffing Piles",
-    TakeAllStuffingPilesDesc = "Takes every stuffing pile if found.",
-   
+    TakeAllItemsDesc = "Takes all of the items in the map with the chosen rarity.",
     GriefOneItemTitle = "Grief One Item",
-    GriefOneItemDesc = "Takes one of the items on the map and teleports it to somewhere inaccessible.",
+    GriefOneItemDesc "Takes one of the items in the map with the chosen rarity and then teleports it to somewhere inaccessible.",
     GriefAllItemsTitle = "Grief All Items",
-    GriefAllItemsDesc = "Takes all of the items on the map and teleports them to somewhere inaccessible.",
+    GriefAllItemsDesc = "Takes all of the items in the map with the chosen rarity and then teleports it to somewhere inaccessible.",
     
-    ProtectionGroupboxTitle = "Protection",
-    ProtectionGroupboxDesc = "Utilities about protection.",
+    SafetyGroupboxTitle = "Protection",
+    SafetyGroupboxDesc = "Features about your safety.",
+    
+    OffsetThresholdTitle = "Offset Threshold",
+    OffsetThresholdDesc = "The offset value for the rejects god mode, it is preferred to turn on Smart Offset under this slider instead of playing with this value.",
     ImmunityRejectsTitle = "Rejects God Mode",
-    ImmunityRejectsDesc = "Makes you immune to the rejects, making them not be able to see you neither attack you if they see you so.",
+    ImmunityRejectsDesc = "Makes you immune to every reject and their attacks.",
     ImmunityMeistroTitle = "Meistro God Mode",
     ImmunityMeistroDesc = "Makes you immune to Meistro's attacks.",
+    AutoHiFlyTitle = "HiFly God Mode",
+    AutoHiFlyDesc = "Makes so you automatically like HiFly's peak music.",
     
-    -- visuals --
-    PerformanceGroupboxTitle = "Performance",
-    PerformanceGroupboxDesc = "Utilities about performance.",
+    UtilityGroupboxTitle = "Utility",
+    UtilityGroupboxDesc = "Features about game utilities.",
+    DesyncTitle = "Desync",
+    DesyncDesc = "Makes so people still see you .",
     
-    DisableRenderingTitle = "Disable Rendering",
-    DisableRenderingDesc = "Disables 3D rendering to improve performance and prevent overheating.",
-    
-    -- automation --
+    -- Automation
     CalibrationGroupboxTitle = "Calibration",
-    CalibrationGroupboxDesc = "Machine based utilities.",
-
+    CalibrationGroupboxDesc = "Features about extracting machines.",
     AutoDefaultMachTitle = "Auto Calibrate Default Machines",
-    AutoDefaultMachDesc = "Automatically calibrates the default machines for you.",
+    AutoDefaultMachDesc = "Automatically does the skillcheck for default machines.",
     AutoDroneMachTitle = "Auto Calibrate Drone Machines",
-    AutoDroneMachDesc = "Automatically calibrates the drone machines that appear in the gardenview for you.",
-
-    AutofarmGroupboxTitle = "Autofarm",
-    AutofarmGroupboxDesc = "Plays the game for you.",
+    AutoDroneMachDesc = "Automatically does the skillcheck for drone machines.",
+    
     ToggleAutofarmTitle = "Toggle Autofarm",
-    ToggleAutofarmDesc = "Toggles the auto-farm, if you want to save peformance to prevent disconnecting and overheating, you can turn on the ‘Disable Rendering’ feature located in Visuals > Performance.",
-    Automation_UtilitiesGroupboxTitle = "Utilities",
-    Automation_UtilitiesGroupboxDesc = "Overall utilities automated.",
-    AutoTakeStuffingPileTitle = "Auto Take Stuffing Piles",
-    AutoTakeStuffingPileDesc = "Automatically takes the stuffing piles on panic.",
-    AutoTeleportTrainTitle = "Auto Teleport To Train",
-    AutoTeleportTrainDesc = "Automatically teleports to the train on panic.",
---[[
-    -- visuals --
-    PerformanceGroupboxTitle = "Performance",
-    DisableRenderingTitle = "Disable Rendering",
-    DisableRenderingDescription = "Saves power and performance.",
+    ToggleAutofarmDesc = "Automatically plays the game for you, expect this feature to have minor bugs.",
     
-    -- automation --
-    
-    
-    
-    -- errors --
-    PlayerHumanoidMissing = "Where'd your humanoid go?",
-    PlayerDead = "You aren't alive!",
-    PlayerAlive = "You aren't dead!",
-    PlayerNotSpectating = "You aren't spectating anybody!",
-    
-    -- notifications --
-    RenderingEnabled = "Rendering enabled!",
-    RenderingDisabled = "Rendering disabled!",
-    ]]
+    -- TEMPORARY
+    WhereAreStuffTitle = "WHERE'S MOST OF THE SCRIPT!?",
+    WhereAreStuffDesc = "This version only supports Main, Player & Automation tabs, the others are still in progress, blame the owner for being a lazy fuck. This version is highly unstable, report the bugs you find in the tester bug reports, where else you'd get this script anyways.\n\n I may just quit."
 }
