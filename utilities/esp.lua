4729ed5d22dfef2c39eb811b1fc81afb865516f9
christmas-cookie <smellinggrassdaily@gmail.com>
2026-07-15 00:07:39 +0300

    Update esp.lua

--- utilities/esp.lua
+++ utilities/esp.lua
@@ -4,7 +4,7 @@ local ESP = {}
 local Actives = {}
 
 local Settings = {
-	FrameSize = UDim2.fromScale(1, 1),
+	FrameSize = 0.8,
 	Highlights = true,
 	HighlightOutlineTransparency = 0.2,
 	HighlightFillTransparency = 0.6,
@@ -35,6 +35,7 @@ function ESP:Add(Data)
 		CustomName = Data.CustomName or TargetInstance.Name,
 		Maid = ActiveMaid
 	}
+	
 	local Billboard = Instance.new("BillboardGui")
 	Billboard.Name = "linear's billboard"
 	Billboard.AlwaysOnTop = true
@@ -42,22 +43,30 @@ function ESP:Add(Data)
 	Billboard.LightInfluence = 0
 	Billboard.MaxDistance = math.huge
 	Billboard.ResetOnSpawn = true
-	Billboard.Size = UDim2.new(7, 0, 7, 0)
+	Billboard.Size = UDim2.new(0, 2000, 0, 2000)
 	Billboard.ZIndexBehavior = Enum.ZIndexBehavior.Global
 	Billboard.Enabled = Settings.Billboards
 	Billboard.Adornee = TargetInstance
 	Billboard.Parent = TargetInstance
 	ActiveMaid:GiveTask(Billboard)
 	Active.Billboard = Billboard
+	
 	local Container = Instance.new("Frame")
 	Container.Name = "Container"
 	Container.BackgroundTransparency = 1
 	Container.AnchorPoint = Vector2.new(0.5, 0.5)
 	Container.Position = UDim2.new(0.5, 0, 0.5, 0)
-	Container.Size = Settings.FrameSize
+	Container.Size = UDim2.new(1, 0, 1, 0)
 	Container.Style = Enum.FrameStyle.Custom
 	Container.Parent = Billboard
 	Active.Container = Container
+	
+	local UiScale = Instance.new("UIScale")
+	UiScale.Name = "UIScale"
+	UiScale.Scale = Settings.FrameSize
+	UiScale.Parent = Container
+	Active.UIScale = UiScale
+
 	local UiListLayout = Instance.new("UIListLayout")
 	UiListLayout.Name = "UIListLayout"
 	UiListLayout.FillDirection = Enum.FillDirection.Vertical
@@ -66,32 +75,39 @@ function ESP:Add(Data)
 	UiListLayout.SortOrder = Enum.SortOrder.LayoutOrder
 	UiListLayout.Padding = UDim.new(0, 0)
 	UiListLayout.Parent = Container
+	
 	local Title = Instance.new("TextLabel")
 	Title.Name = "Title"
 	Title.BackgroundTransparency = 1
-	Title.Font = Enum.Font.Merriweather
+	Title.Font = Enum.Font.SourceSansSemibold
 	Title.LayoutOrder = 0
-	Title.Size = UDim2.new(1, 0, 0.150000006, 0)
+	Title.Size = UDim2.new(0, 0, 0, 20)
+	Title.AutomaticSize = Enum.AutomaticSize.X
 	Title.Text = Active.CustomName
 	Title.TextColor3 = Active.Color
-	Title.TextScaled = true
-	Title.TextWrapped = true
+	Title.TextScaled = false
+	Title.TextSize = 22
+	Title.TextWrapped = false
 	Title.TextXAlignment = Enum.TextXAlignment.Center
 	Title.TextYAlignment = Enum.TextYAlignment.Center
 	Title.TextStrokeColor3 = Color3.new(0, 0, 0)
 	Title.TextStrokeTransparency = 0
 	Title.Parent = Container
 	Active.Title = Title
+	
 	local Divider = Instance.new("Frame")
 	Divider.Name = "Divider"
 	Divider.BackgroundColor3 = Active.Color
 	Divider.BorderSizePixel = 0
 	Divider.LayoutOrder = 1
-	Divider.Size = UDim2.new(0.699999988, 0, 0.00600000005, 0)
+	Divider.AnchorPoint = Vector2.new(0.5, 1)
+	Divider.Position = UDim2.new(0.55, 0, 1.1, 0)
+	Divider.Size = UDim2.new(1.1, 0, 0.059, 0)
 	Divider.Style = Enum.FrameStyle.Custom
 	Divider.Visible = false
-	Divider.Parent = Container
+	Divider.Parent = Title
 	Active.Divider = Divider
+	
 	local UiStroke = Instance.new("UIStroke")
 	UiStroke.Name = "UIStroke"
 	UiStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Contextual
@@ -99,6 +115,26 @@ function ESP:Add(Data)
 	UiStroke.Thickness = 0.5
 	UiStroke.LineJoinMode = Enum.LineJoinMode.Round
 	UiStroke.Parent = Divider
+	
+	local TagsFrame = Instance.new("Frame")
+	TagsFrame.Name = "Tags"
+	TagsFrame.BackgroundTransparency = 1
+	TagsFrame.Size = UDim2.new(1, 0, 0, 0)
+	TagsFrame.AutomaticSize = Enum.AutomaticSize.Y
+	TagsFrame.LayoutOrder = 4
+	TagsFrame.Style = Enum.FrameStyle.Custom
+	TagsFrame.Parent = Container
+	Active.TagsFrame = TagsFrame
+
+	local TagsListLayout = Instance.new("UIListLayout")
+	TagsListLayout.Name = "UIListLayout"
+	TagsListLayout.FillDirection = Enum.FillDirection.Vertical
+	TagsListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
+	TagsListLayout.VerticalAlignment = Enum.VerticalAlignment.Top
+	TagsListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+	TagsListLayout.Padding = UDim.new(0, -3)
+	TagsListLayout.Parent = TagsFrame
+
 	local Highlight = Instance.new("Highlight")
 	Highlight.Name = "linear's highlight"
 	Highlight.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
@@ -111,6 +147,7 @@ function ESP:Add(Data)
 	Highlight.Parent = TargetInstance
 	ActiveMaid:GiveTask(Highlight)
 	Active.Highlight = Highlight
+	
 	table.insert(Actives, Active)
 end
 
@@ -137,9 +174,9 @@ function ESP:Tag(Data)
 			local TagLabel = Instance.new("TextLabel")
 			TagLabel.Name = "Tag_Example"
 			TagLabel.BackgroundTransparency = 1
-			TagLabel.Font = Enum.Font.Ubuntu
+			TagLabel.Font = Enum.Font.SourceSans
 			TagLabel.LayoutOrder = 3
-			TagLabel.Size = UDim2.new(9, 0, 0.108999997, 0)
+			TagLabel.Size = UDim2.new(0, 700, 0, 20)
 			TagLabel.Text = Data.Name
 			TagLabel.TextColor3 = Active.Color
 			TagLabel.TextScaled = true
@@ -148,7 +185,7 @@ function ESP:Tag(Data)
 			TagLabel.TextYAlignment = Enum.TextYAlignment.Center
 			TagLabel.TextStrokeColor3 = Color3.new(0, 0, 0)
 			TagLabel.TextStrokeTransparency = 0
-			TagLabel.Parent = Active.Container
+			TagLabel.Parent = Active.TagsFrame
 			Active.Maid:GiveTask(TagLabel)
 			Active.Tags[Data.Flag] = TagLabel
 			CanDivider(Active)
@@ -207,7 +244,7 @@ function ESP:Settings(Data)
 	if Data.HighlightFillTransparency ~= nil then Settings.HighlightFillTransparency = Data.HighlightFillTransparency end
 	if Data.Billboards ~= nil then Settings.Billboards = Data.Billboards end
 	for fart, Active in ipairs(Actives) do
-		if Active.Container then Active.Container.Size = Settings.FrameSize end
+		if Active.UIScale then Active.UIScale.Scale = Settings.FrameSize end
 		if Active.Billboard then Active.Billboard.Enabled = Settings.Billboards end
 		if Active.Highlight then
 			Active.Highlight.Enabled = Settings.Highlights
